d4380828c99c8df7ae9d4a64bffe87bdc46d3871
Chris Underwood <cunderwoodmn@gmail.com>
2016-07-10 18:28:41 -0700

    completed project and initial files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var name= "Charlie"
+var age= 25
+var hometown= "Los Angeles, CA"
+var occupation = "designer"
+var favoriteFood = "pasta"
+var bestFriend = "Tom"
+var favoriteColor = "black"
+var hobby1 = "coding"
+var hobby2 ="birds"
+
+
+console.log("His name is " + name + ".");
+console.log("He is " + age + " years old and is from " + hometown + ". ");
+console.log("For work he is a " + occupation + ".");
+console.log("His favoite food is " + favoriteFood + ", best friend is " + bestFriend + ", and his favorite color is " + favoriteColor + ".");
+console.log("Two of " + name + "'s hobbies are " + hobby1 + " and " + hobby2 + ".");
